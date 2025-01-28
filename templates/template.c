73d2241a973823a13829360261fcbb7c19cfda10
Donal Morrissey <donal.morrissey@42t.com>
2025-01-28 13:05:43 +0000

    Initial commit of various template files.

---
+++ templates/template.c
@@ -0,0 +1,90 @@
+/**
+ * @file xxxxx.c
+ * @brief Project XXXXX Application source file
+ * 
+ * @copyright (c) Horizon TCL. 2025. All rights reserved.
+ * All rights reserved. Copying, compilation, modification, distribution or
+ * any other use whatsoever of this material is strictly prohibited except in
+ * accordance with a Software License Agreement.
+ */
+
+/**
+ * @brief XXXXX Module Implementation
+ * @details See Header file for full details
+ * 
+ * @date XX
+ * @author XXXXX
+ * @version XX
+ */
+
+/**
+ * @defgroup CompilationSwitches Compilation Switches
+ * @{
+ */
+
+// None
+
+/** @} */
+
+/**
+ * @defgroup Includes Module Includes
+ * @{
+ */
+
+// None
+
+/** @} */
+
+/**
+ * @defgroup Constants Module Constants
+ * @{
+ */
+
+/** @} */
+
+/**
+ * @defgroup Types Module Type Definitions
+ * @{
+ */
+
+// None
+
+/** @} */
+
+/**
+ * @defgroup PrivateFunctions Private Function Prototypes
+ * @{
+ */
+
+// None
+
+/** @} */
+
+/**
+ * @defgroup LocalStorage Local Storage
+ * @{
+ */
+
+// None
+
+/** @} */
+
+/**
+ * @defgroup PrivateFunctionImplementations Private Function Implementations
+ * @{
+ */
+
+// None
+
+/** @} */
+
+/**
+ * @defgroup PublicFunctions Public Function Implementations
+ * @{
+ */
+
+// None
+
+/** @} */
+
+/* End of file */
---
+++ templates/template.h
@@ -0,0 +1,59 @@
+/**
+ * @file xxxxx.h
+ * @brief Project XXXXX Application header file
+ * 
+ * @copyright (c) Horizon TCL. 2025. All rights reserved.
+ * All rights reserved. Copying, compilation, modification, distribution or
+ * any other use whatsoever of this material is strictly prohibited except in
+ * accordance with a Software License Agreement.
+ */
+
+/**
+ * @brief XXXXX Module Interface
+ * @details <Purpose of .h and .c files for this module>
+ *
+ * @date XX
+ * @author XXXXX
+ * @version XX
+ */
+
+#ifndef XXXXX_H
+#define XXXXX_H
+
+/**
+ * @defgroup Includes Module Includes
+ * @{
+ */
+
+// None
+
+/** @} */
+
+/**
+ * @defgroup Constants Module Constants
+ * @{
+ */
+
+// None
+
+/** @} */
+
+/**
+ * @defgroup Types Module Type Definitions  
+ * @{
+ */
+
+// None
+
+/** @} */
+
+/**
+ * @defgroup Functions Public Function Prototypes
+ * @{
+ */
+
+// None
+
+/** @} */
+
+#endif /* XXXXX_H */
